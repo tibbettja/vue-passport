5ee6bf01e45ba4a4338427419bf502402f1d488f
Tibbett <davisjb2@appstate.edu>
2019-08-17 21:36:24 -0400

    fixed some vuex

--- src/store/modules/application.js
+++ src/store/modules/application.js
@@ -1,9 +1,7 @@
-import { HTTP } from '../../common/http-common'
+// import { HTTP } from '../../common/http-common'
 
 const state = {
   errors: [],
-  username: '',
-  admin: false,
   window: {}
 }
 
@@ -21,36 +19,10 @@ const mutations = {
   },
   'RESIZE' (state, data) {
     state.window = data
-  },
-  // Auth
-  'SET_USERNAME' (state, data) {
-    state.username = data.username
-    state.isAdmin = data.isAdmin
   }
 }
 
 const actions = {
-  // auth
-  async newUser ({commit}, { name, pass }) {
-    var userRes = await HTTP.post(`/user/create`, { username: name, password: pass })
-    if (userRes.data.status === 200) {
-      commit('SET_USERNAME', { username: userRes.data.username, isAdmin: false })
-      return { success: true }
-    }
-    /* eslint-disable-next-line no-console */
-    console.error('There was an error when Creating a new Account', userRes)
-    commit('ERROR', 'Error when Creating a new Account')
-  },
-  async login ({commit}, { name, pass }) {
-    var userRes = await HTTP.post(`/user/login`, { username: name, password: pass })
-    if (userRes.data.status === 200) {
-      commit('SET_USERNAME', { username: userRes.data.result.username, isAdmin: userRes.data.result.isAdmin })
-      return { success: true }
-    }
-    /* eslint-disable-next-line no-console */
-    console.error('There was an error when Logging in', userRes)
-    commit('ERROR', 'Error when Logging in')
-  },
   popError ({commit}) {
     commit('POP_ERROR')
   },
@@ -65,9 +37,6 @@ const actions = {
 const getters = {
   getErrors (state) {
     return state.errors
-  },
-  isAdmin (state) {
-    return state.isAdmin
   },
   getWindow (state) {
     return state.window
--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import application from './modules/application'
+import user from './modules/user'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   strict: false,
   modules: {
-    application
+    application,
+    user
   }
 })
