967adf7263bf971c7824c8b4479a17c02696b79c
Tibbett <davisjb2@appstate.edu>
2019-08-17 20:38:56 -0400

    routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,51 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import landing from '@/components/landing'
+import signup from '@/components/signup'
+import login from '@/components/login'
+import logout from '@/components/logout'
+import account from '@/components/account'
+import home from '@/components/home'
+import forohfor from '@/components/forohfor'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/welcome',
+      name: 'landing',
+      component: landing
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: signup
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      component: logout
+    },
+    {
+      path: '/account',
+      name: 'account',
+      component: account
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: home
+    },
+    {
+      path: '*',
+      name: 'forohfor',
+      component: forohfor
     }
   ]
 })
